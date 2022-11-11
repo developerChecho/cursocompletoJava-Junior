c694cedcb6d2d942c6e8bb9efea57038593b761e
sergiop <chenchoxg2@hotmail.com>
2022-11-11 14:09:28 -0500

    se aprende concepto de entradas y salidas utilizando variables

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+     /*ENTRADA Y SALIDA
+      String name;
+      Integer edad;
+      String profesion;
+      Integer añoexperiencia;
+
+       name = JOptionPane.showInputDialog("digite su nombre");
+       edad = Integer.parseInt(JOptionPane.showInputDialog("digite su edad"));
+       profesion = JOptionPane.showInputDialog("Cual es su profesion");
+       añoexperiencia =Integer.parseInt(JOptionPane.showInputDialog("cuantos años tiene de experiencia"));
+        JOptionPane.showMessageDialog(null,"su nombre es " + name );
+        JOptionPane.showMessageDialog(null,"su edad es " +edad);
+        JOptionPane.showMessageDialog(null,"su profesion es " +profesion);
+        JOptionPane.showMessageDialog(null," tienes " +añoexperiencia + "años de experiencia"); */
+    }
+
+}
